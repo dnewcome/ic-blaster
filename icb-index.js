7d7c65ead0d41a82636fa5f24cb26f0bfd194b51
Dan Newcome <dan@dnuke.com>
2015-01-30 22:54:38 -0800

    adding ability to search in desc, cleanup

--- icb-index.js
+++ icb-index.js
@@ -1,13 +1,11 @@
-var jsdom = require('jsdom');
-var assert = require('assert');
+var jsdom = require('jsdom'),
+	assert = require('assert'),
+	fs = require('fs'),
+	util = require('util');
 
-var fs = require('fs');
-var util = require('util');
-
-var library = [];
-var filesprocessed = 0;
-
-var files = fs.readdirSync( __dirname + '/giicm/html' );
+var library = [],
+	filesprocessed = 0,
+	files = fs.readdirSync( __dirname + '/giicm/html' );
 
 function done() {
 	if( filesprocessed == files.length ) {
@@ -16,30 +14,33 @@ function done() {
 }
 
 for( var i=0; i<files.length; i++ ) {
-	parse( fs.readFileSync( __dirname + '/giicm/html/' + files[i], 'utf-8'  ) ); 
+	parse(fs.readFileSync(__dirname + '/giicm/html/' + files[i], 'utf-8'));
 }
 
-
-function parse( html ) {
-  jsdom.env( html, [ 'http://code.jquery.com/jquery-1.5.min.js' ],
+function parse(html) {
+  jsdom.env( html, ['http://code.jquery.com/jquery-1.5.min.js'],
     function(errors, window) {
-		var parts =  window.document.getElementsByTagName( "h2" ); 
-		var pinouts =  window.document.getElementsByTagName( "pre" ); 
-		assert.equal( parts.length, pinouts.length, "parts and pinouts don't match" );
+		var parts = window.document.getElementsByTagName("h2"),
+			pinouts = window.document.getElementsByTagName("pre"),
+			part,
+			desc,
+			pinout;
+
+		assert.equal(parts.length, pinouts.length, "parts and pinouts don't match");
 
 		for( var i=0; i< parts.length; i++ ) {
-			var part = parts[i].textContent;
-			var desc = parts[i].nextSibling.textContent.replace( /\n/g,'' );
-			var pinout = pinouts[i].textContent;
+			part = parts[i].textContent;
+			desc = parts[i].nextSibling.textContent.replace(/\n/g, '');
+			pinout = pinouts[i].textContent;
 			try {
-				assert.notEqual( part, '' );
-				assert.notEqual( desc, '' );
-				assert.notEqual( pinout, '' );
+				assert.notEqual(part, '');
+				assert.notEqual(desc, '');
+				assert.notEqual(pinout, '');
 			}
-			catch( e ) {
-				console.log( 'error' );
+			catch(e) {
+				console.log('error');
 			}
-			library.push( { pn: part, desc: desc, pinout: pinout } );
+			library.push({pn: part, desc: desc, pinout: pinout});
 		}
 		filesprocessed++;
 		done();
--- icb-query.js
+++ icb-query.js
@@ -1,13 +1,23 @@
-var fs = require('fs');
+var fs = require('fs'),
+	library = JSON.parse(fs.readFileSync(__dirname + '/library.json', 'utf-8')),
+	term = process.argv[2],
+	item,
+	re;
 
+if(!term) {
+	console.log('usage: icb {part|desc}');
+	process.exit();
+}
+
+// yes, print a blank line 
 console.log('');
-var library = JSON.parse( fs.readFileSync( __dirname + '/library.json', 'utf-8' ) );
 
-for( var i=0; i<library.length; i++ ) {
-	if( library[i].pn == process.argv[2] ) {
+for(var i = 0; i < library.length; i++) {
+	item = library[i],
+	re = new RegExp(term);
+
+	if (item.pn === term || re.test(item.desc) === true) {
 		console.log( library[i].desc );
 		console.log( library[i].pinout );
 	}	
-	else {
-	}
 }
